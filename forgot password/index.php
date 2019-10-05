210aa9e8e29346dc3e6a8cbce097f68b5f7f54e8
ElijahWale <30737833+ElijahWale@users.noreply.github.com>
2019-10-05 17:06:39 +0100

    forget password file

---
+++ forgot password/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet"  type="text/css" href="style.css">
+
+    <!-- link to site icon -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+     <link rel="icon" href="favicon.ico" type="image/x-icon">   
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <title>forgot password</title>
+  </head>
+  <body>
+    <div class="container">
+          <div class="row">
+               <div class="col-md-12">
+                    <img src="https://res.cloudinary.com/elijahwale/image/upload/v1570274902/franc_l4rec5.png" style="width: 150px; height: 150px;">
+                    <h1>Reset Password Form</h1>
+                    <div class="card">
+                         <div class="card-body">
+                              <h2 class="card-title">Forgot Password</h2>
+                              <p class="card-text">Enter the Email address associated with your Account for password recovery</p>
+                              <input type="text" class="form-control" placeholder="Your Email Address" id="email">
+                              <button type="button" class="btn">Recover Password</button>
+                         </div>
+                          
+                    </div> 
+               </div>   
+          </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
